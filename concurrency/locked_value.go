56b4330573c7ee1cb62484d63b3409635bd1c618
FMotalleb <fmotalleb@gmail.com>
2025-06-17 15:37:48 +0330

    fix: used RW mutex instead of normal locker

--- concurrency/locked_value.go
+++ concurrency/locked_value.go
@@ -8,31 +8,33 @@ type (
 	Operator[T any]    func(T) T
 	LockedValue[T any] struct {
 		value T
-		lock  sync.Locker
+		mx    *sync.RWMutex
 	}
 )
 
 func NewLockedValue[T any](initial T) *LockedValue[T] {
 	return &LockedValue[T]{
 		value: initial,
-		lock:  &sync.Mutex{},
+		mx:    &sync.RWMutex{},
 	}
 }
 
 func (lv *LockedValue[T]) Get() T {
-	lv.lock.Lock()
-	defer lv.lock.Unlock()
+	lv.mx.RLock()
+	defer lv.mx.RUnlock()
 	return lv.value
 }
 
 func (lv *LockedValue[T]) Set(newValue T) {
-	lv.lock.Lock()
-	defer lv.lock.Unlock()
+	lv.mx.Lock()
+	defer lv.mx.Unlock()
 	lv.value = newValue
 }
 
+// Operate and modify the value in locked state
+
 func (lv *LockedValue[T]) Operate(operator Operator[T]) {
-	lv.lock.Lock()
-	defer lv.lock.Unlock()
+	lv.mx.Lock()
+	defer lv.mx.Unlock()
 	lv.value = operator(lv.value)
 }
