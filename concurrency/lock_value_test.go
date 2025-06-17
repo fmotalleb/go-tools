022d6eb581983718dfab0ae8efe34193e5e06ab2
FMotalleb <fmotalleb@gmail.com>
2025-06-17 15:32:59 +0330

    feat: concurrency helper (locked value)

---
+++ concurrency/lock_value_test.go
@@ -0,0 +1,25 @@
+package concurrency_test
+
+import (
+	"testing"
+
+	"github.com/FMotalleb/go-tools/concurrency"
+	"github.com/alecthomas/assert/v2"
+)
+
+func TestLockedValue(t *testing.T) {
+	// Create a new LockedValue with an initial value of 10
+	lv := concurrency.NewLockedValue[int](10)
+
+	// Test the Get method
+	assert.Equal(t, 10, lv.Get())
+
+	// Test the Set method
+	lv.Set(20)
+	assert.Equal(t, 20, lv.Get())
+
+	// Test the Operate method
+	operator := func(x int) int { return x * 2 }
+	lv.Operate(operator)
+	assert.Equal(t, 40, lv.Get())
+}
