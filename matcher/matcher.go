58a289ed6089f642b9b0a60ab1d696200cb2b3aa
FMotalleb <fmotalleb@gmail.com>
2025-07-02 13:43:53 +0330

    feat: enhance Matcher with Decode method for glob pattern handling

--- matcher/matcher.go
+++ matcher/matcher.go
@@ -1,6 +1,47 @@
 package matcher
 
-type Matcher interface {
+import (
+	"errors"
+	"reflect"
+	"strings"
+
+	"github.com/FMotalleb/go-tools/matcher/glob"
+)
+
+type Matcher struct {
+	matcher
+}
+
+type matcher interface {
 	Match(string) bool
-	Validate() error
+}
+
+func (m *Matcher) Decode(from, _ reflect.Type, val interface{}) (any, error) {
+	var ty, pat string
+	switch from.Kind() {
+	case reflect.String:
+		str := val.(string)
+		split := strings.Split(str, ":")
+		switch len(split) {
+		case 1:
+			ty = "glob"
+			pat = str
+		default:
+			ty = split[0]
+			pat = strings.Join(split[1:], ":")
+		}
+	}
+	switch ty {
+	case "glob":
+		var err error
+		var mat matcher
+		if mat, err = glob.Compile(pat); err != nil {
+			return nil, err
+
+		}
+		m.matcher = mat
+		return m, nil
+	}
+
+	return errors.New("failed to find matcher variant"), nil
 }
